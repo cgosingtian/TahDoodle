c0db696dbdbe3cc67b2840093805f017895d51b3
Chase Gosingtian <c.gosingtian@klab.com>
2014-07-22 16:51:15 +0800

    Initial Commit

---
+++ TahDoodle/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TahDoodle
+//
+//  Created by Chase Gosingtian on 7/22/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
